cd581920a6d0923b9850bcd33fb0d4287103c58d
rufunt <jazzoo@mail.ru>
2020-04-12 16:23:13 +0300

    Add destroy action for tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,17 @@ class TasksController < ApplicationController
   before_action :set_project
 
   def create
-    @task = @project.task.create(task_params)
+    @task = @project.tasks.create(task_params)
+    redirect_to @project
+  end
+
+  def destroy
+    @task = @project.tasks.find(params[:id])
+    if @task.destroy
+      flash[:success] = "task was deleted."
+    else
+      flash[:error] = "task was not deleted."
+    end
     redirect_to @project
   end
 
