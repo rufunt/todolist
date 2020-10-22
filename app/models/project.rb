246933970ce0b518aa91a3de5ac0dc011b51fc4f
rufunt <jazzoo@mail.ru>
2020-10-22 15:47:17 +0300

    Fix validates for tasks and projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,4 +2,6 @@ class Project < ApplicationRecord
   has_many :tasks, dependent: :destroy
 
   validates_presence_of :title, :description
+  validates :title, length: { minimum: 3, maximum: 100 }
+  validates :description, length: { minimum: 3, maximum: 300 }
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -2,6 +2,7 @@ class Task < ApplicationRecord
   belongs_to :project
 
   validates_presence_of :content
+  validates :content, length: { minimum: 3, maximum: 300 }
   
   def completed?
     !completed_at.blank?
