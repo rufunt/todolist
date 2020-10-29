731806017456ed9757b419dcbebb6db89e06530f
rufunt <jazzoo@mail.ru>
2020-10-29 16:21:52 +0300

    Fix ordering tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -34,6 +34,7 @@ class TasksController < ApplicationController
 
   def complete
     @task.update_attribute(:completed_at, Time.now)
+    @task.priority = 0
     redirect_to @project, notice: "Task Completed"
   end
 
